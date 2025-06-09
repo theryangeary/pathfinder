a8943f2e4890e094cc9a57c8d0df4f0f200d8be9
Ryan Geary <rtgnj42@gmail.com>
2025-06-09 09:41:33 -0400

    logo click goes home

--- src/web/components/Logo.tsx
+++ src/web/components/Logo.tsx
@@ -2,11 +2,16 @@ import './Logo.css';
 
 const PathfinderLogo = () => {
   return (
+    <a href="/" style={{
+      color: 'inherit', /* no blue text */
+      textDecoration: 'inherit', /* no underline */
+    }}>
     <div className="logo-container">
         <h1 className="logo-text">
             <span className="pathfinder">PATHFINDER</span><span className="dot">.</span><span className="prof">prof</span>
         </h1>
     </div>
+    </a>
   );
 };
 
